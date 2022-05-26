5aed2c64072c49c3c3fcfffb02591c747048bd31
eric shin <ericshin7824@gmail.com>
2022-05-26 14:52:25 -0700

    divided from my portfolio web

--- dice-game.js
+++ dice-game.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
-const playerZero = document.querySelector("#player-0");
-const playerOne = document.querySelector("#player-1");
+const playerZero = document.querySelector('#player-0');
+const playerOne = document.querySelector('#player-1');
 
-const screenZero = document.querySelector("#screen-0");
-const screenOne = document.querySelector("#screen-1");
+const screenZero = document.querySelector('#screen-0');
+const screenOne = document.querySelector('#screen-1');
 
-const totalZero = document.querySelector("#total-0");
-const totalOne = document.querySelector("#total-1");
+const totalZero = document.querySelector('#total-0');
+const totalOne = document.querySelector('#total-1');
 
-const currentZero = document.querySelector("#current-0");
-const currentOne = document.querySelector("#current-1");
+const currentZero = document.querySelector('#current-0');
+const currentOne = document.querySelector('#current-1');
 
-const diceZero = document.querySelector(".dice-0");
-const diceOne = document.querySelector(".dice-1");
+const diceZero = document.querySelector('.dice-0');
+const diceOne = document.querySelector('.dice-1');
 
-const startZero = document.querySelector("#start-0");
-const startOne = document.querySelector("#start-1");
+const startZero = document.querySelector('#start-0');
+const startOne = document.querySelector('#start-1');
 
 let scores, currentScore, activePlayer, playing;
 
@@ -48,17 +48,17 @@ const init = function () {
                                 <span>Win socre : 20</span>
                             </p>`;
 
-    playerZero.classList.remove("winner");
-    playerOne.classList.remove("winner");
+    playerZero.classList.remove('winner');
+    playerOne.classList.remove('winner');
 
-    playerZero.classList.remove("loser");
-    playerOne.classList.remove("loser");
+    playerZero.classList.remove('loser');
+    playerOne.classList.remove('loser');
 
-    playerZero.classList.remove("active");
-    playerOne.classList.remove("active");
+    playerZero.classList.remove('active');
+    playerOne.classList.remove('active');
 
-    playerZero.classList.remove("inactive");
-    playerOne.classList.remove("inactive");
+    playerZero.classList.remove('inactive');
+    playerOne.classList.remove('inactive');
 };
 init();
 
@@ -75,25 +75,25 @@ function btnStart(e) {
         currentZero.textContent = 0;
         currentOne.textContent = 0;
 
-        if (playerOne.classList.contains("winner") || playerZero.classList.contains("winner")) {
-            playerZero.classList.remove("winner");
-            playerOne.classList.remove("winner");
-            playerZero.classList.remove("loser");
-            playerOne.classList.remove("loser");
+        if (playerOne.classList.contains('winner') || playerZero.classList.contains('winner')) {
+            playerZero.classList.remove('winner');
+            playerOne.classList.remove('winner');
+            playerZero.classList.remove('loser');
+            playerOne.classList.remove('loser');
         }
 
         if (e != startOne) {
             // select the first player
             activePlayer = 0;
 
-            playerZero.classList.add("active");
-            playerOne.classList.add("inactive");
+            playerZero.classList.add('active');
+            playerOne.classList.add('inactive');
         } else if (e != startZero) {
             // select the first player
             activePlayer = 1;
 
-            playerOne.classList.add("active");
-            playerZero.classList.add("inactive");
+            playerOne.classList.add('active');
+            playerZero.classList.add('inactive');
         }
     }
 }
@@ -104,14 +104,14 @@ const switchPlayer = function () {
     activePlayer = activePlayer === 0 ? 1 : 0;
 
     document.querySelector(`#current-${activePlayer}`).textContent = 0;
-    playerZero.classList.toggle("active");
-    playerOne.classList.toggle("active");
+    playerZero.classList.toggle('active');
+    playerOne.classList.toggle('active');
 
-    playerZero.classList.toggle("inactive");
-    playerOne.classList.toggle("inactive");
+    playerZero.classList.toggle('inactive');
+    playerOne.classList.toggle('inactive');
 
-    playerZero.classList.remove("invalid");
-    playerOne.classList.remove("invalid");
+    playerZero.classList.remove('invalid');
+    playerOne.classList.remove('invalid');
 };
 
 function btnRoll(e) {
@@ -127,11 +127,11 @@ function btnRoll(e) {
             document.querySelector(`#current-${activePlayer}`).textContent = currentScore;
         } else {
             document.querySelector(`#current-${activePlayer}`).innerHTML = `DEAD NUMBER<br>${dice}`;
-            document.querySelector(`#player-${activePlayer}`).classList.add("invalid");
+            document.querySelector(`#player-${activePlayer}`).classList.add('invalid');
 
             setTimeout(() => {
                 // set delay
-                document.querySelector(`#player-${activePlayer}`).classList.remove("invalid");
+                document.querySelector(`#player-${activePlayer}`).classList.remove('invalid');
                 // swich to next player
                 switchPlayer();
             }, 1200);
@@ -151,22 +151,22 @@ function btnHold(e) {
             // if score is <= 30, plaing = false to finish the game
             playing = false;
 
-            playerZero.classList.remove("active");
-            playerOne.classList.remove("active");
-            playerZero.classList.remove("inactive");
-            playerOne.classList.remove("inactive");
+            playerZero.classList.remove('active');
+            playerOne.classList.remove('active');
+            playerZero.classList.remove('inactive');
+            playerOne.classList.remove('inactive');
 
             if (scores[0] > scores[1]) {
-                playerZero.classList.add("winner");
+                playerZero.classList.add('winner');
                 currentZero.textContent = `You win!`;
 
-                playerOne.classList.add("loser");
+                playerOne.classList.add('loser');
                 currentOne.textContent = `You lose!`;
             } else {
-                playerOne.classList.add("winner");
+                playerOne.classList.add('winner');
                 currentOne.textContent = `You win!`;
 
-                playerZero.classList.add("loser");
+                playerZero.classList.add('loser');
                 currentZero.textContent = `You lose!`;
             }
             setTimeout(() => {
@@ -180,12 +180,12 @@ function btnHold(e) {
 
         // if rolled 1, display message.
     } else if (playing && !currentScore > 0 && e.id.slice(-1) == activePlayer) {
-        document.querySelector(`#current-${activePlayer}`).textContent = "ROLL THE DICE !";
-        document.querySelector(`#player-${activePlayer}`).classList.add("invalid");
+        document.querySelector(`#current-${activePlayer}`).textContent = 'ROLL THE DICE !';
+        document.querySelector(`#player-${activePlayer}`).classList.add('invalid');
 
         // set delay
         setTimeout(() => {
-            document.querySelector(`#player-${activePlayer}`).classList.remove("invalid");
+            document.querySelector(`#player-${activePlayer}`).classList.remove('invalid');
             document.querySelector(`#current-${activePlayer}`).textContent = currentScore;
         }, 1200);
     }
